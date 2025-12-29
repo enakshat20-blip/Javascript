388067a3b93238dddd89b56388554254ff08b624
enakshat20-blip <enakshat20@gmail.com>
2025-12-29 11:49:05 +0000

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert( 3+3 ) // we are using node JS not browser
+
+console.log(3
+    +
+     3)  // code readablity sgould be high
+
+
+console.log("Akshat")
+
+let name = "Akshat"
+let age = 24
+let isloggedIn = true
+
+// number int => 2 to power 53
+// bigint
+// string => ""
+// boolean => True/False 
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+// object
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
