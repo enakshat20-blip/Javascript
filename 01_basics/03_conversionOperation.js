68a7c036076043f67dbdb1efa66ae81a472ae4b3
enakshat20-blip <enakshat20@gmail.com>
2026-01-11 10:53:14 +0000

    conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,39 @@ let SomeNumber = 33
 let StringNumber = String(SomeNumber)
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+// ************************* Operations ************************* 
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+//console.log negative value
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Akshat"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true)
+// console.log(+"")
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2; // not ideal way code should be readable
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
