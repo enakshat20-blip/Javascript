749d86411bdfa5e5b82e591ce27ebb8260e0827b
enakshat20-blip <enakshat20@gmail.com>
2026-01-11 09:07:23 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)// type is number
+// null => 0
+// undefiend => NaN
+// boolean true,false => 1,0
+
+let isLoggedIn = "Akshat"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Akshat" => true
+
+let SomeNumber = 33
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
