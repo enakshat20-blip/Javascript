ff8e74eddf731a8099fc6a59aabb10b97cdb8cfe
enakshat20-blip <enakshat20@gmail.com>
2026-03-15 16:50:26 +0530

    Update 04_comparison.js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,20 +1,22 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
 
-// console.log("2" > 1);
-// console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0); // == take as it but comparison <,>,=<,>= convert null to no. 0 this is why it is true 
+// console.log("2">1);
+// console.log("02">1);
 
-console.log(undefined == 0);
-console.log(undefined < 0);
-console.log(undefined > 0);
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
 
-// === (Strict check) - also check data type
+/* the reason is equality == check and the comapriosn <,>,>= works differently.
+comparison treat null as 0 thats why null==0 is false and null>=0 is true*/
 
-console.log("2" === 2);
+
+// === strict check this will check datatypes too
+
+console.log("2" == 2); // True because is convert "2" to no and then comapre it 
+console.log("2" === 2); //false because it dont convert "2" to no. and check its datatype too
