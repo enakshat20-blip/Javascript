3fe4d476c62f05d69a388c333e5e468bc9ac3c48
enakshat20-blip <enakshat20@gmail.com>
2026-03-19 18:09:19 +0000

    numbs

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,13 @@
+const score = 400
+console.log(score)
+const balance = new Number(1000)
+console.log(balance);
+
+console.log(balance.toString()) // to change no. to string  
+console.log(balance.toString().length) // to also get length
+console.log(balance.toFixed(5)) // to get precise value 
+
+const otherNumber = 122.8933
+
+console.log(otherNumber.toPrecision(3)); // to get precision roundoff the output will be 123
+
