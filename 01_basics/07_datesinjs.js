bf584e39955de7368dafa1d649e27a97671c6996
enakshat20-blip <enakshat20@gmail.com>
2026-03-22 05:51:31 +0000

    dates

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -13,15 +13,32 @@
 
 
 // let myCreateddate = new Date(2026, 0, 20)
-//let myCreateddate = new Date(2026, 0, 20, 10, 25, 15)
-//console.log(myCreateddate.toLocaleString());
+// let myCreateddate = new Date(2026, 0, 20, 10, 25, 15)
+// console.log(myCreateddate.toLocaleString());
 
 let myCreateddate = new Date("01-14-2026")
 // console.log(myCreateddate.toLocaleString());
 
 let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(myCreateddate.getTime());
-console.log(Date.now()/1000) // to convert it into seconds from milliseconds but will give decimal value 
-console.log(Math.floor(Date.now()/1000)) // to convert it into seconds from millisecond and remove decimal by using math.floor
+// console.log(myTimeStamp);
+// console.log(myCreateddate.getTime());
+// console.log(Date.now()/1000) // to convert it into seconds from milliseconds but will give decimal value 
+// console.log(Math.floor(Date.now()/1000)) // to convert it into seconds from millisecond and remove decimal by using math.floor
 
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+// console.log(newDate.getYear());
+// console.log(newDate.getDate());
+// console.log(newDate.getTime());
+
+
+// to use in industry
+//`${newDate.getDay} and the time is ${newDate.getTime}`
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+    month: "long",
+})
+);
